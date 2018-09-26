5f5f293bd0abc10201d151acce6e6688534e7291
priyaljain919 <priyal.jain@pepcus.com>
2018-09-26 14:05:21 +0530

    life @ pepcus slider added demo

--- js/script.js
+++ js/script.js
@@ -1,22 +1,14 @@
 $(document).ready(function(){
-	$('.owl-demo').owlCarousel({
-	      navigation : false, // Show next and prev buttons
-	      slideSpeed : 300,
-	      singleItem:true
-	})
-	// $('.owl-demo').owlCarousel({
-	// navigation: true,
-	// loop:true,
- //    responsive:{
- //        0:{
- //            items:1
- //        },
- //        600:{
- //            items:1
- //        },
- //        1000:{
- //            items:1
- //        }
- //    }
-	// });
+	 var owl = $(".owl-demo");
+     owl.owlCarousel({
+       items: 1,
+       itemsDesktop: [1000, 4],
+       itemsDesktopSmall: [900, 2],
+       itemsTablet: [600, 1],
+       itemsMobile: false,
+       pagination: false,
+       autoplay:true,
+	   autoplayTimeout:5000,
+	   autoplayHoverPause:true
+     });
 });
