3b58acec31ebde522da3b36b32dab0ece985ea28
priyaljain919 <priyal.jain@pepcus.com>
2018-09-26 16:01:48 +0530

    vedio and grid content added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-	 var owl = $(".owl-demo");
-     owl.owlCarousel({
-       items: 1,
-       itemsDesktop: [1000, 4],
-       itemsDesktopSmall: [900, 2],
-       itemsTablet: [600, 1],
-       itemsMobile: false,
-       pagination: false,
-       autoplay:true,
-	   autoplayTimeout:5000,
-	   autoplayHoverPause:true
-     });
-});
+// $(document).ready(function(){
+// 	 var owl = $(".owl-demo");
+//      owl.owlCarousel({
+//        items: 1,
+//        itemsDesktop: [1000, 4],
+//        itemsDesktopSmall: [900, 2],
+//        itemsTablet: [600, 1],
+//        itemsMobile: false,
+//        pagination: false,
+//        autoplay:true,
+// 	   autoplayTimeout:5000,
+// 	   autoplayHoverPause:true
+//      });
+// });
