e7c9d56120809a6d5e500ab97c28c2da886046a2
priyaljain919 <priyal.jain@pepcus.com>
2018-09-26 13:15:40 +0530

    sonam pic added

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$('.owl-demo').owlCarousel({
+	      navigation : false, // Show next and prev buttons
+	      slideSpeed : 300,
+	      singleItem:true
+	})
+	// $('.owl-demo').owlCarousel({
+	// navigation: true,
+	// loop:true,
+ //    responsive:{
+ //        0:{
+ //            items:1
+ //        },
+ //        600:{
+ //            items:1
+ //        },
+ //        1000:{
+ //            items:1
+ //        }
+ //    }
+	// });
+});
