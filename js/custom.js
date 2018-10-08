c8b9cc6f2084dfa803749b0344391a6b59b3976c
priyaljain919 <priyal.jain@pepcus.com>
2018-10-08 19:11:41 +0530

    removed overflow scroll css

--- js/custom.js
+++ js/custom.js
@@ -7,19 +7,32 @@ $(document).ready(function(e) {
         if ($(this).scrollTop() > 100) {
             $('.scrollup').fadeIn();
             $('.header-menu').addClass("change-height");
+            // $('body').css("position","absolute");
+            // $('body').css("overflow-y","scroll");
+            // $('body').css("bottom","0");
+            // $('.scrollup').css("display","block");
+            // console.log(window.scrollTop);
+            // $('body').css("position","absolute");
         } else {
             $('.scrollup').fadeOut();
+            // $('body').css("position","static");
             $('.header-menu').removeClass("change-height");
         }
     });
+     $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            // $('body').css("position","absolute");
+           
+        }
+    });
     $('.scrollup').click(function () {
-        $("html, body").animate({
+        $("html,body,section").animate({
             scrollTop: 0
         }, 600);
         return false;
     });
     var scrollLink = $('.scroll');
-  
+    
   // Smooth scrolling
   scrollLink.click(function(e) {
     e.preventDefault();
