398b428212cb6ad879207e07a0656e5da35ae501
priyaljain919 <priyal.jain@pepcus.com>
2018-10-16 11:12:23 +0530

    footer content changed

--- js/custom.js
+++ js/custom.js
@@ -56,5 +56,39 @@ $(document).ready(function(e) {
     
 });
 
+var page = document.getElementById('page');
+var sections = page.getElementsByTagName('section');
+// This transition can be defined in the CSS if preferred.
+var transition = 'top 0.8s ease-in-out 0s';
+page.style.transition = transition;
+page.onclick = slideDown;
 
+function slideDown(e) {
+
+  // Delegate.
+  if (e.target.className != 'next') {
+    return;
+  }
+  
+  // Prevent firing simultaneously.
+  page.onclick = '';
+  self = e.target.parentNode;
+  var offset = self.getBoundingClientRect();
+  var scroll = self.offsetTop;
+
+  // CSS Transition slide.
+  page.style.top = (-offset.height-offset.top) + 'px';
+
+  setTimeout(function () {
+    // Reposition the real scrollbar.
+    page.style.transition = 'none';
+    page.style.top = '';
+    window.scrollTo(0, offset.height+scroll);
+    page.style.transition = transition;
+    // Reattach event.
+    page.onclick = slideDown;
+    
+    // This timeout length should match the CSS animation time (.8s).
+  }, 800);
+}
   
