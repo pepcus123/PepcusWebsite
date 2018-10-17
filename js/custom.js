99173ecbb7d583af469af272fd6e446cc4ffe158
priyaljain919 <priyal.jain@pepcus.com>
2018-10-17 13:19:33 +0530

    removed unused media queries

--- js/custom.js
+++ js/custom.js
@@ -49,10 +49,10 @@ $(document).ready(function(e) {
     //         }
     //     $(this).toggleClass('.hover');
     // });
-     $('.team-container').on('click', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('.hover');
-    });
+    //  $('.team-container').on('click', function(e) {
+    //     e.preventDefault();
+    //     $(this).toggleClass('.hover');
+    // });
     
 });
 
